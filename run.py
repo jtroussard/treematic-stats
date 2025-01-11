bcb50c2f3b2f7514df29cf29c790c6b3be8d064b
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2025-01-11 16:23:30 -0500

    adds github actions and updates main logic to exit if there is no change in data

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from updater import update_installs_data
+import argparse
 
 def fetch_installs():
     url = "https://marketplace.visualstudio.com/items?itemName=DevLife4Me.tree-meh"
@@ -14,12 +15,17 @@ def fetch_installs():
     installs_text = soup.find("span", {"class": "installs-text"}).text.strip().split(" ")[0]
     installs_number = installs_text.replace(",", "")  # Remove commas for clean numbers
 
-    print(f"Fetched Installs: {installs_number}")
-    return installs_number
+    return int(installs_number)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--check-only', action='store_true', help="Only fetch installs without updating data.json")
+    args = parser.parse_args()
+
     installs = fetch_installs()
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    
-    # Update data.json on GitHub with the new installs count
-    update_installs_data(date=current_date, installs=installs)
+
+    if args.check_only:
+        print(installs)  # For GitHub Action check step
+    else:
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        update_installs_data(date=current_date, installs=installs)
